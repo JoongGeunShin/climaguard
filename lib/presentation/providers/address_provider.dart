35f2ddce884ac736f6a68b5d8623df34e8f57611
SJG <shinjg97@gmail.com>
2026-06-29 22:55:48 +0900

    [update] nominatim 역지오코딩

--- lib/presentation/providers/address_provider.dart
+++ lib/presentation/providers/address_provider.dart
@@ -1,4 +1,3 @@
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 import '../../core/utils/reverse_geocoder.dart';
@@ -9,35 +8,29 @@ part 'address_provider.g.dart';
 
 /// 현재 위치의 행정구역 정보를 반환합니다.
 ///
+/// OpenStreetMap Nominatim API 사용 (API 키 불필요).
 /// locationProvider가 갱신되면 자동으로 재계산됩니다.
-/// Kakao API 키가 없거나 요청 실패 시 null을 반환합니다(앱 진행에 영향 없음).
 @riverpod
 Future<AdminDistrict?> adminDistrict(AdminDistrictRef ref) async {
   final position = await ref.watch(locationProvider.future);
   final dio = ref.watch(dioProvider);
-  final apiKey = dotenv.env['KAKAO_REST_API_KEY'] ?? '';
 
   return ReverseGeocoder.toAdminDistrict(
     position.latitude,
     position.longitude,
     dio: dio,
-    apiKey: apiKey,
   );
 }
 
 /// 현재 위치의 도로명 주소를 반환합니다.
-///
-/// 상세 주소 화면 등에서 필요할 때 선택적으로 사용합니다.
 @riverpod
 Future<RoadAddress?> roadAddress(RoadAddressRef ref) async {
   final position = await ref.watch(locationProvider.future);
   final dio = ref.watch(dioProvider);
-  final apiKey = dotenv.env['KAKAO_REST_API_KEY'] ?? '';
 
   return ReverseGeocoder.toRoadAddress(
     position.latitude,
     position.longitude,
     dio: dio,
-    apiKey: apiKey,
   );
 }
--- lib/presentation/providers/address_provider.g.dart
+++ lib/presentation/providers/address_provider.g.dart
@@ -6,12 +6,12 @@ part of 'address_provider.dart';
 // RiverpodGenerator
 // **************************************************************************
 
-String _$adminDistrictHash() => r'94a5a92464e4cd8534d358fa51d851aed8157eb5';
+String _$adminDistrictHash() => r'e642923f47e8496ddcf31b5c5f02d9c62bca155b';
 
 /// 현재 위치의 행정구역 정보를 반환합니다.
 ///
+/// OpenStreetMap Nominatim API 사용 (API 키 불필요).
 /// locationProvider가 갱신되면 자동으로 재계산됩니다.
-/// Kakao API 키가 없거나 요청 실패 시 null을 반환합니다(앱 진행에 영향 없음).
 ///
 /// Copied from [adminDistrict].
 @ProviderFor(adminDistrict)
@@ -29,11 +29,9 @@ final adminDistrictProvider =
 @Deprecated('Will be removed in 3.0. Use Ref instead')
 // ignore: unused_element
 typedef AdminDistrictRef = AutoDisposeFutureProviderRef<AdminDistrict?>;
-String _$roadAddressHash() => r'2b77d62c45458dd151427c05b86ebd558f0a07f9';
+String _$roadAddressHash() => r'41eb9dd900b791500f66d55de906868bfe619934';
 
 /// 현재 위치의 도로명 주소를 반환합니다.
-///
-/// 상세 주소 화면 등에서 필요할 때 선택적으로 사용합니다.
 ///
 /// Copied from [roadAddress].
 @ProviderFor(roadAddress)
