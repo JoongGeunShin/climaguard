04ef9ce2beed42f4b77492c3e45d7c30b5292e9f
SJG <shinjg97@gmail.com>
2026-06-29 12:35:38 +0900

    [feat] dio provider

---
+++ lib/data/datasources/dio_provider.dart
@@ -0,0 +1,16 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'dio_provider.g.dart';
+
+@Riverpod(keepAlive: true)
+Dio dio(Ref ref) {
+  return Dio(
+    BaseOptions(
+      connectTimeout: const Duration(seconds: 10),
+      receiveTimeout: const Duration(seconds: 10),
+      responseType: ResponseType.json,
+    ),
+  );
+}
