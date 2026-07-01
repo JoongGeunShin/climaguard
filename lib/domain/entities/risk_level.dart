7209c2ee96b20be92ac9b79a03f7771c14b27c8f
SJG <shinjg97@gmail.com>
2026-07-01 17:28:00 +0900

    [update] attention 추가 (safe↔caution 사이), label: '관심'

--- lib/domain/entities/risk_level.dart
+++ lib/domain/entities/risk_level.dart
@@ -1,13 +1,15 @@
 enum RiskLevel {
   safe,
+  attention,
   caution,
   warning,
   danger;
 
   String get label => switch (this) {
-        RiskLevel.safe => '안전',
-        RiskLevel.caution => '주의',
-        RiskLevel.warning => '경고',
-        RiskLevel.danger => '위험',
+        RiskLevel.safe      => '안전',
+        RiskLevel.attention => '관심',
+        RiskLevel.caution   => '주의',
+        RiskLevel.warning   => '경고',
+        RiskLevel.danger    => '위험',
       };
 }
